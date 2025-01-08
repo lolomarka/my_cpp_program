a4ca3836f68f59139ca40a70d734c273f81c0a6b
lolomarka <lolomarka@gmail.com>
2025-01-08 12:07:12 +0500

    Simple ui hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <QApplication>
+#include <QLabel>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QLabel label("Hello, World!"); // Создаем виджет QLabel с текстом
+    label.setAlignment(Qt::AlignCenter); // Выравниваем текст по центру
+    label.resize(400, 200); // Устанавливаем размер окна
+    label.show(); // Отображаем окно
+
+    return app.exec(); // Запускаем главный цикл приложения
 }
