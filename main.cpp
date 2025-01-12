367c66711b453c2c86e794a8a889316afb75c584
lolomarka <lolomarka@gmail.com>
2025-01-12 13:28:54 +0500

    reset to cmd

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
-#include <QApplication>
-#include <QLabel>
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-
-    QLabel label("Hello, World!"); // Создаем виджет QLabel с текстом
-    label.setAlignment(Qt::AlignCenter); // Выравниваем текст по центру
-    label.resize(400, 200); // Устанавливаем размер окна
-    label.show(); // Отображаем окно
-
-    return app.exec(); // Запускаем главный цикл приложения
+#include <iostream>
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
